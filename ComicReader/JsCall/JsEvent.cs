7c6ffdafd5bf5301e0ed5f8e20a319449b62d117
kelin-xycs <kelin_xycs@163.com>
2019-05-01 22:29:47 +0800

    添加项目文件。

---
+++ ComicReader/JsCall/JsEvent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using Xilium.CefGlue;
+
+namespace ComicReader.JsCall
+{
+    public class JsEvent
+    {
+        public void OpenFolderDialog()
+        {
+            CefProcessMessage m = CefProcessMessage.Create("OpenFolderDialog");
+
+            DemoRenderProcessHandler._browser.SendProcessMessage(CefProcessId.Browser, m);
+        }
+
+        public string GetImages(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new Exception("path 不能为 空 。");
+
+            string[] urlList = Directory.GetFiles(path);
+
+            string url;
+            string ext;
+            List<string> urlList2 = new List<string>();
+
+            for (int i=0; i<urlList.Length; i++)
+            {
+                url = urlList[i];
+                ext = Path.GetExtension(url);
+
+                if (ext == ".bmp" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif" || ext == ".png")
+                    urlList2.Add("http://" + ToLocalFileUrl(url));
+            }
+
+            return ToJson(urlList2);
+        }
+
+        private string ToLocalFileUrl(string path)
+        {
+            return "localfile/" + path.Replace(":", string.Empty).Replace("\\", "/");
+        }
+
+        private string ToJson(List<string> list)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[");
+
+            for (int i=0; i<list.Count; i++)
+            {
+                sb.Append("'");
+                sb.Append(list[i]);
+                sb.Append("',");
+            }
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+    }
+}
